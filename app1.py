795b617e356c1d6edbc244bd497782a5293f51cc
NA <rahs0120@gmail.com>
2020-02-15 14:59:28 +0900

    버튼메시지 수정

--- app1.py
+++ app1.py
@@ -6,7 +6,7 @@ root = tk.Tk()
 root.geometry("200x100")
 
 lbl = tk.Label(text="LABEL")
-btn = tk.Button(text="PUSH", command=dispLabel)
+btn = tk.Button(text=" 클릭하세요", command=dispLabel)
 
 lbl.pack()
 btn.pack()
